105c7441fec42887f39d81e0edff71b267f4fd6c
Isaac Lapides <IsaacLapides@gmail.com>
2020-06-23 14:47:16 -0400

    Updates

---
+++ precache-manifest.4227fec6fda0945b8561753cd047d849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132263b539edb4c44dcb204351e43891",
+    "url": "/assignment4/index.html"
+  },
+  {
+    "revision": "04f83ebba9d16cda941b",
+    "url": "/assignment4/static/css/main.20ff9de4.chunk.css"
+  },
+  {
+    "revision": "dbb06751a7ee6380db66",
+    "url": "/assignment4/static/js/2.662aa0b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment4/static/js/2.662aa0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f83ebba9d16cda941b",
+    "url": "/assignment4/static/js/main.86662dcb.chunk.js"
+  },
+  {
+    "revision": "2cbeb271593ac73150dd",
+    "url": "/assignment4/static/js/runtime-main.f27fb323.js"
+  }
+]);
